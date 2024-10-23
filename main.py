f51542e15cf3e6afbc37b075b8c538b9b6eee216
Sabyasachi1310 <86130630+Sabyasachi1310@users.noreply.github.com>
2024-10-24 03:49:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -823,3 +823,5 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    port = int(os.environ.get("PORT", 5000))  # Render will provide the PORT environment variable
+    app.run(host='0.0.0.0', port=port)
