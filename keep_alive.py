2b61c3913d44b87e863145f5ac3670fe5639a647
Sabyasachi1310 <86130630+Sabyasachi1310@users.noreply.github.com>
2024-10-24 11:40:38 +0600

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from Flask import Flask, render template
+from threading import Thread
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+  return "Alive"
+
+def run():
+  app.run(host='0.0.0.0',port-8080)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
